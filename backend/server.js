faf8ed5e5b67155f197f701e9462a9fb62fdaccd
Htet Aung Shine <42883024+htetaungshine2111@users.noreply.github.com>
2026-05-09 21:26:04 -0700

    configure cors for deployment

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,13 @@ const dashboardRoutes = require("./routes/dashboardRoutes");
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    credentials: true,
+  }),
+);
+
 app.use(express.json());
 
 app.use("/", authRoutes);
